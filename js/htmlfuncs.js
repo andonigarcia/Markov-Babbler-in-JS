444ea801e53244b46aa028089b164b84ec42d0db
Andoni Garcia <andoni@uchicago.edu>
2014-12-01 12:45:03 -0600

    Some debugging messages

--- js/htmlfuncs.js
+++ js/htmlfuncs.js
@@ -13,7 +13,6 @@ function settingUp(text){
 }
 
 function goBabble(){
-	document.body.style.cursor = "wait";
 	document.getElementById("toBabble").innerHTML = "Parsing the text...";
 	var huck = huckleberryFinnText();
 	var t = settingUp(huck);
@@ -21,6 +20,5 @@ function goBabble(){
 	var bab = babble(5, 5, t);
 	var text = bab.join("\n");
 	document.getElementById("toBabble").innerHTML = text;
-	document.body.style.cursor = "auto";
 	return;
 }

e8e3b05f69fadb184592b91224c9b271d14617b3
Andoni Garcia <andoni@uchicago.edu>
2014-12-01 12:55:03 -0600

    Lets take it from step 1 forwards. Dont put the cart infront of the horse

--- js/htmlfuncs.js
+++ js/htmlfuncs.js
@@ -15,11 +15,11 @@ function settingUp(text){
 function goBabble(){
 	var msg1 = "Parsing the text..."
 	document.getElementById("toBabble").innerHTML = msg1;
-	var huck = huckleberryFinnText();
+	/*var huck = huckleberryFinnText();
 	var t = settingUp(huck);
 	document.getElementById("toBabble").innerHTML = "Babbling...";
 	var bab = babble(5, 5, t);
 	var text = bab.join("\n");
 	document.getElementById("toBabble").innerHTML = text;
-	return;
+	return;*/
 }

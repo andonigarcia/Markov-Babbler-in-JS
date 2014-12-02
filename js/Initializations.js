358f39e01b1c5b8b37593550267ffb4695c3c6d0
Andoni Garcia <andoni@uchicago.edu>
2014-12-01 21:22:08 -0600

    I think I fixed the paragraphing problems

--- js/Initializations.js
+++ js/Initializations.js
@@ -10,17 +10,17 @@ function init(file){
 
 function start(sent, par){
 	var bab = babble(par, sent);
-	var div = document.createElement("div");
+
+	var parent = document.getElementById("writeHere");
+	while(parent.firstChild){
+		parent.removeChild(parent.firstChild);
+	}
 
 	for(var i = 0; i < bab.length; i++){
 		var text = bab[i];
 		var para = document.createElement("p");
 		var node = document.createTextNode(text);
 		para.appendChild(node);
-		div.appendChild(para);
+		parent.appendChild(para);
 	}
-	var parent = document.getElementById("writeHere");
-	var child = document.getElementById("child");
-	parent.replaceChild(div, child);
-
 }

7264c0e82cc49a976f512b39295a941ceefae365
Andoni Garcia <andoni@uchicago.edu>
2014-12-03 02:04:04 -0600

    Couldnt figure it out. Trying to quicken backend

--- js/Initializations.js
+++ js/Initializations.js
@@ -3,6 +3,20 @@
 // =====================================================================
 // ========================= Initializations ===========================
 // =====================================================================
+var TABLEARR = [];
+
+function isInitialized(s){
+	var t = [false, null];
+	for(var i = 0; i < TABLEARR.length; i++){
+		var x = TABLEARR[i];
+		if(x[0] === "Huck"){
+			t[0] = true;
+			t[1] = x[1];
+			return t;
+		}
+	}
+	return t;
+}
 
 function start(sent, par){
 	var bab = babble(par, sent);
